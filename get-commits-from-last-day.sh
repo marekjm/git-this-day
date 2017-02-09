68fbd4c0914d9fd1b6969ca14f1e6c9b1b0b414b
Marek Marecki <marek.marecki@posbit.pl>
2017-02-09 18:22:30 +0100

    Wait until the output is complete, then print it all together

--- get-commits-from-last-day.sh
+++ get-commits-from-last-day.sh
@@ -10,4 +10,14 @@ git checkout $1 &> /dev/null
 if [[ $VERBOSE == 'true' ]]; then
     echo "checking branch $(git rev-parse --abbrev-ref HEAD)"
 fi
-git log --oneline --author "$GIT_THIS_DAY_USER_EMAIL" --since 16hours
+
+GIT_THIS_DAY_BRANCH_REPORT=$(git log --oneline --author "$GIT_THIS_DAY_USER_EMAIL" --since 16hours)
+
+if [[ $GIT_THIS_DAY_BRANCH_REPORT != '' ]]; then
+    WHITE="\033[1;37m"
+    LIGHT_GREEN="\033[1;32m"
+    RESET="\033[0m"
+    echo -e "[$LIGHT_GREEN""note$RESET] on branch '$WHITE$BRANCH$RESET'" >> /tmp/git-this-day.txt
+    echo "$GIT_THIS_DAY_BRANCH_REPORT" >> /tmp/git-this-day.txt
+    echo '' >> /tmp/git-this-day.txt
+fi
--- what-have-i-done-today.sh
+++ what-have-i-done-today.sh
@@ -12,5 +12,9 @@ GIT_THIS_DAY_PATH=/usr/local/lib/git-this-day
 STARTING_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "[note] starting branch: $STARTING_BRANCH"
 
+echo '' > /tmp/git-this-day.txt
+
 git branch --sort=-committerdate --verbose | head -n "$GIT_THIS_DAY_DEPTH" | sed 's/^\*/ /' | awk '{ print $1 }' | sort | uniq | xargs -n 1 $GIT_THIS_DAY_PATH/get-commits-from-last-day.sh
 git checkout "$STARTING_BRANCH" &> /dev/null
+
+cat /tmp/git-this-day.txt
