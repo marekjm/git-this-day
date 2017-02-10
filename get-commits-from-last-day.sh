4a08cf710e04bd4ec72ba14d8c1d680bbb0519aa
Marek Marecki <marek.marecki@posbit.pl>
2017-02-10 17:03:47 +0100

    Small timeout before reprinting

--- get-commits-from-last-day.sh
+++ get-commits-from-last-day.sh
@@ -18,6 +18,8 @@ if [[ $GIT_THIS_DAY_BRANCH_REPORT != '' ]]; then
     echo '' >> /tmp/git-this-day.txt
     echo -e "[$GIT_THIS_DAY_LIGHT_GREEN""note$GIT_THIS_DAY_RESET] on branch '$GIT_THIS_DAY_WHITE$BRANCH$GIT_THIS_DAY_RESET'" >> /tmp/git-this-day.txt
     echo "$GIT_THIS_DAY_BRANCH_REPORT" >> /tmp/git-this-day.txt
+else
+    sleep '0.05s'
 fi
 
 echo -ne "\033[1G\033[0K"
